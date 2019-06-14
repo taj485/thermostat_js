20f61cc6c617d0070be9a4e9c63ab2c67f941e30
Taj <tajbi21@gmail.com>
2019-06-14 11:25:31 +0100

    weather API working

--- view/thermostatJquery.js
+++ view/thermostatJquery.js
@@ -3,6 +3,14 @@ $( document ).ready(function() {
   screenUpdate();
   displayPowerUsage();
 
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp)
+    })
+  })
+
   $( ".temperature" ).text(thermostat.temperature);
 
   $( ".up" ).on('click', function() { // event listener
